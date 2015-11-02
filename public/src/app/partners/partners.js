0116aa36b42a790fd5c02b2aace63291e3ceb727
ArcheGalla <artempushkar@gmail.com>
2015-11-02 18:31:51 +0200

    tmmp commit basic refactoring

---
+++ public/src/app/partners/partners.js
@@ -0,0 +1,9 @@
+angular.module("app").directive("partners", [function () {
+  return {
+    restrict: "E",
+    templateUrl: "app/partners/partners.html",
+    link: function ($scope, element, attrs) {
+
+    }
+  }
+}]);
--- public/src/app/registration/registration.js
+++ public/src/app/registration/registration.js
@@ -4,6 +4,29 @@ angular.module("app").directive("registration", [function () {
     templateUrl: "app/registration/registration.html",
     link: function ($scope, element, attrs) {
 
+      $scope.form = {
+        name: "",
+        sname: "",
+        email: "",
+        phone: null,
+        message: ""
+      };
+
+      $scope.submit = function (form, isValid) {
+        console.log(form);
+        if (isValid) {
+          console.log(form);
+          //$http.
+          //  post("api/registration", {form: form}).
+          //  then(function (response) {
+          //    console.info("form submit successful");
+          //  }).catch(function (err) {
+          //    console.log(err);
+          //  });
+        }
+      };
+
+
     }
   }
 }]);
