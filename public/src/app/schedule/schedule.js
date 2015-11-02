6a35e7698ec2e9cbe892415615b236fbd4b1e0c7
ArcheGalla <artempushkar@gmail.com>
2015-11-02 04:16:17 +0200

    preview version

--- public/src/app/schedule/schedule.js
+++ public/src/app/schedule/schedule.js
@@ -5,94 +5,90 @@ angular.module("app").directive("schedule", [function () {
     link: function ($scope, element, attrs) {
       $scope.schedule = [
         {
-          icon: "fa-anchor",
+          icon: "fa-pencil-square-o",
           text: "Реєстрація",
-          time: {
-            start: "9:00",
-            end: ""
-          }
+          bg_color: "#e7b300",
+          font_color: "#fff",
+          time: "9:00"
         },
         {
-          icon: "fa-anchor",
+          icon: "fa-university",
           text: "Відкриття симпозіуму",
-          time: {
-            start: "9:45",
-            end: ""
-          }
+          bg_color: "#fec503",
+          font_color: "#fff",
+          time: "9:45"
         },
         {
-          icon: "fa-anchor",
+          icon: "fa-microphone",
           text: "Ι сесія",
-          time: {
-            start: "10:00",
-            end: "11:30"
-          }
+          bg_color: "#ffdd6a",
+          font_color: "#fff",
+          time: "10:00 - 11:30"
         },
         {
-          icon: "fa-anchor",
+          icon: "fa-coffee",
           text: "Перерва на каву",
-          time: {
-            start: "11:30",
-            end: "12:00"
-          }
-        },
+          bg_color: "#ffffff",
+          font_color: "#fec503",
+          time: "11:30 - 12:00"
+        }
+        ,
         {
-          icon: "fa-anchor",
+          icon: "fa-microphone",
           text: "ΙΙ сесія ",
-          time: {
-            start: "12:00",
-            end: "13:45"
-          }
-        },
+          bg_color: "#ffdd6a",
+          font_color: "#fff",
+          time: "12:00 - 13:45"
+        }
+        ,
         {
-          icon: "fa-anchor",
+          icon: "fa-cutlery",
           text: "Обід",
-          time: {
-            start: "13:45",
-            end: "15:00"
-          }
-        },
+          bg_color: "#fff",
+          font_color: "#fec503",
+          time: "13:45 - 15:00"
+        }
+        ,
         {
-          icon: "fa-anchor",
+          icon: "fa-microphone",
           text: "ΙΙΙ сесія",
-          time: {
-            start: "15:00",
-            end: "16:30"
-          }
-        },
+          bg_color: "#ffdd6a",
+          font_color: "#fff",
+          time: "15:00 - 16:30"
+        }
+        ,
         {
-          icon: "fa-anchor",
+          icon: "fa-coffee",
           text: "Перерва на каву",
-          time: {
-            start: "16:30",
-            end: "17:00"
-          }
-        },
+          bg_color: "#ffffff",
+          font_color: "#fec503",
+          time: "16:30-17:00"
+        }
+        ,
         {
-          icon: "fa-anchor",
+          icon: "fa-microphone",
           text: "ΙV сесія",
-          time: {
-            start: "17:00",
-            end: "18:30"
-          }
-        },
+          bg_color: "#ffdd6a",
+          font_color: "#fff",
+          time: "17:00-18:30 "
+        }
+        ,
         {
-          icon: "fa-anchor",
+          icon: "fa-university",
           text: "Закриття симпозіуму",
-          time: {
-            start: "18:30",
-            end: "18:45"
-          }
-        },
+          bg_color: "#fec503",
+          font_color: "#fff",
+          time: "18:30-18:45"
+        }
+        ,
         {
-          icon: "fa-anchor",
+          icon: "fa-glass",
           text: "Вечеря",
-          time: {
-            start: "",
-            end: "20:00"
-          }
-        }];
-
+          bg_color: "#e7b300",
+          font_color: "#fff",
+          time: "20:00"
+        }
+      ];
     }
   }
 }]);
