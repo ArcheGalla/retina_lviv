2678de16916aed1b760b688c15142d0db000ec6b
ArcheGalla <artempushkar@gmail.com>
2015-11-09 00:50:32 +0200

    add new text and links

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
@@ -16,11 +18,12 @@ app.use(cookieParser());
 app.get("/test", function (req, res) {
   res.send("Hello World! ");
 });
+
 app.use('/', express.static(__dirname + '/public/dist/'));
 
 
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-  console.log("work");
+  console.log(" magic happen on " + port);
 });
--- public/src/app/schedule/schedule.js
+++ public/src/app/schedule/schedule.js
@@ -7,86 +7,70 @@ angular.module("app").directive("schedule", [function () {
         {
           icon: "fa-pencil-square-o",
           text: "Реєстрація",
-          bg_color: "#ffdd6a",
-          //font_color: "#000",
           time: "9:00"
         },
         {
           icon: "fa-university",
           text: "Відкриття симпозіуму",
-          bg_color: "#fff",
-          //font_color: "#fec503",
           time: "9:45"
         },
         {
           icon: "fa-microphone",
           text: "Ι сесія",
-          bg_color: "#ffdd6a",
-          //font_color: "#fff",
           time: "10:00 - 11:30"
         },
         {
           icon: "fa-coffee",
           text: "Перерва на каву",
-          bg_color: "#ffffff",
-          //font_color: "#fec503",
           time: "11:30 - 12:00"
         }
         ,
         {
           icon: "fa-microphone",
           text: "ΙΙ сесія ",
-          bg_color: "#ffdd6a",
-          //font_color: "#fff",
           time: "12:00 - 13:45"
         }
         ,
         {
           icon: "fa-cutlery",
           text: "Обід",
-          bg_color: "#fff",
-          //font_color: "#fec503",
-          time: "13:45 - 15:00"
+          time: "13:45 - 14:45"
+        }
+        ,
+        {
+          icon: "fa-microphone",
+          text: "Лекція",
+          time: "14:45 - 15:15"
         }
         ,
         {
           icon: "fa-microphone",
           text: "ΙΙΙ сесія",
-          bg_color: "#ffdd6a",
-          //font_color: "#fff",
-          time: "15:00 - 16:30"
+          time: "15:30 - 17:00"
         }
         ,
         {
           icon: "fa-coffee",
           text: "Перерва на каву",
-          bg_color: "#ffffff",
-          //font_color: "#fec503",
-          time: "16:30-17:00"
+          time: "17:00 - 17:30"
         }
         ,
         {
           icon: "fa-microphone",
           text: "ΙV сесія",
-          bg_color: "#ffdd6a",
-          //font_color: "#fff",
-          time: "17:00-18:30 "
+          time: "17:30-19:00 "
         }
         ,
         {
           icon: "fa-university",
           text: "Закриття симпозіуму",
-          bg_color: "#fff",
-          //font_color: "#fec503",
-          time: "18:30-18:45"
+          time: "19:00-19:15"
         }
         ,
         {
           icon: "fa-glass",
           text: "Вечеря",
-          bg_color: "#ffdd6a",
-          //font_color: "#fff",
-          time: "20:00"
+          time: "20:30"
         }
       ];
     }
