b3c016e36e853e6aed1aa7c1a4590d95756622fb
ArcheGalla <artempushkar@gmail.com>
2015-11-11 11:32:22 +0200

    refactor registration form

--- public/src/app/registration/registration.js
+++ public/src/app/registration/registration.js
@@ -1,5 +1,5 @@
-angular.module("app").directive("registration", ["$timeout", "$http",
-  function ($timeout, $http) {
+angular.module("app").directive("registration", ["$timeout", "$http", "$log",
+  function ($timeout, $http, $log) {
     return {
       restrict: "E",
       templateUrl: "app/registration/registration.html",
@@ -19,20 +19,26 @@ angular.module("app").directive("registration", ["$timeout", "$http",
         };
 
         $scope.submit = function (form, isValid) {
-          $http.post("/register", JSON.stringify(form)).then(function (response) {
-            if (response.status === 200) {
-              $scope.form = {
-                name: "",
-                sname: "",
-                email: "",
-                phone: undefined,
-                message: ""
-              };
-            }
-            $scope.btnText = "SUBMIT_BTN_SUCCESS";
-          });
-        };
+          if (isValid) {
+            $log.info("form valid");
+            console.log("form is valid");
+            $http.post("/register", JSON.stringify(form)).then(function (response) {
+              if (response.status === 200) {
+                $log.info("request success");
+                $scope.form = {
+                  name: "",
+                  sname: "",
+                  email: "",
+                  phone: undefined,
+                  message: ""
+                };
+                $scope.btnText = "SUBMIT_BTN_SUCCESS";
+              }
+            });
+          }
 
+
+        }
       }
     }
   }]);
