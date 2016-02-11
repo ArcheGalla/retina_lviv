657f2c7b9140997467f1bb0fac15a202fd09cde7
ArcheGalla <artempushkar@gmail.com>
2016-02-11 23:33:41 +0200

    test create build process

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,20 @@ var forever = require('forever-monitor');
 
 wrench
   .readdirSyncRecursive('./gulp')
-  .filter(function(file) { return (/\.(js|coffee)$/i).test(file); })
-  .map(function(file) { require('./gulp/' + file); });
+  .filter(function (file) {
+    return (/\.(js|coffee)$/i).test(file);
+  })
+  .map(function (file) {
+    require('./gulp/' + file);
+  });
 
-gulp.task("default", ["clean","build"], function () { });
-gulp.task("build_production", ["clean", "build"], function () { });
-gulp.task("start",function () { new forever.Monitor("app.js").start(); });
+gulp.task("default", ["clean", "build"], function () {
+});
+
+gulp.task("build", ["clean", "build"], function () {
+	console.log("build is complate");
+});
+
+gulp.task("start", function () {
+  new forever.Monitor("app.js").start();
+});
