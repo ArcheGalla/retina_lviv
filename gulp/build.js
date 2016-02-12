6123cdd403faa9c774fa56af71e80e2df297aa17
ArcheGalla <artempushkar@gmail.com>
2016-02-12 19:45:25 +0200

    update build process

--- gulp/build.js
+++ gulp/build.js
@@ -1,29 +1,27 @@
 'use strict';
-
 var path = require('path');
 var gulp = require('gulp');
 var conf = require('./conf');
+var htmlmin = require('gulp-htmlmin');
 
 var $ = require('gulp-load-plugins')({
   pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license', 'del']
 });
 
-gulp.task('partials', function () {
+gulp.task('partials',function () {
   return gulp.src([
     path.join(conf.paths.src, '/app/**/*.html'),
     path.join(conf.paths.tmp, '/serve/app/**/*.html')
   ])
-    .pipe($.minifyHtml({
-      empty: true,
-      spare: true,
-      quotes: true
-    }))
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('public/dist'))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
       module: 'retinaLviv',
       root: 'app'
     }))
     .pipe(gulp.dest(conf.paths.tmp + '/partials/'));
 });
+
 
 gulp.task('html', ['inject', 'partials'], function () {
   var partialsInjectFile = gulp.src(path.join(conf.paths.tmp, '/partials/templateCacheHtml.js'), { read: false });
@@ -54,12 +52,7 @@ gulp.task('html', ['inject', 'partials'], function () {
     .pipe($.useref())
     .pipe($.revReplace())
     .pipe(htmlFilter)
-    .pipe($.minifyHtml({
-      empty: true,
-      spare: true,
-      quotes: true,
-      conditionals: true
-    }))
+    .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(htmlFilter.restore())
     .pipe(gulp.dest(path.join(conf.paths.dist, '/')))
     .pipe($.size({ title: path.join(conf.paths.dist, '/'), showFiles: true }));
@@ -81,7 +74,7 @@ gulp.task('other', function () {
 
   return gulp.src([
     path.join(conf.paths.src, '/**/*'),
-    path.join('!' + conf.paths.src, '/**/*.{css,js,scss}')
+    path.join('!' + conf.paths.src, '/**/*.{css,js,scss,html}')
   ])
     .pipe(fileFilter)
     .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
