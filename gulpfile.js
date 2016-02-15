d49c98b7b463757f66f02bee7defbe45abbaf101
ArcheGalla <artempushkar@gmail.com>
2016-02-15 03:41:41 +0200

    back up with new feature

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,12 @@
 var gulp = require('gulp');
 var wrench = require('wrench');
+var forever = require('forever-monitor');
 
 wrench
   .readdirSyncRecursive('./gulp')
-  .filter(function (file) {
-    return (/\.(js|coffee)$/i).test(file);
-  })
-  .map(function (file) {
-    require('./gulp/' + file);
-  });
+  .filter(function(file) { return (/\.(js|coffee)$/i).test(file); })
+  .map(function(file) { require('./gulp/' + file); });
 
-gulp.task("production", ["clean", "build"], function () {
-  console.log("build is complate");
-});
-
+gulp.task("default", ["clean","build"], function () { });
+gulp.task("build_production", ["clean", "build"], function () { });
+gulp.task("start",function () { new forever.Monitor("app.js").start(); });
