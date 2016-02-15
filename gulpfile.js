0e6fcb5083462404c0b6f4f7b5d4511969ff6ac0
ArcheGalla <artempushkar@gmail.com>
2016-02-15 03:47:46 +0200

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ wrench
   .filter(function(file) { return (/\.(js|coffee)$/i).test(file); })
   .map(function(file) { require('./gulp/' + file); });
 
+
 gulp.task("default", ["clean","build"], function () { });
 gulp.task("build_production", ["clean", "build"], function () { });
 gulp.task("start",function () { new forever.Monitor("app.js").start(); });
