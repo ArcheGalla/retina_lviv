59962eecc7e329e146011123872d284e7b6ca30c
ArcheGalla <artempushkar@gmail.com>
2016-02-25 00:50:41 +0200

    update partners

--- src/app/partners/partners.js
+++ src/app/partners/partners.js
@@ -1,6 +1,25 @@
-angular.module("app").directive("partners", [function () {
-  return {
-    restrict: "E",
-    templateUrl: "app/partners/partners.html"
-  }
-}]);
+angular
+  .module("app")
+  .directive("partners", [function () {
+    return {
+      restrict: "E",
+      templateUrl: "app/partners/partners.html"
+    }
+  }])
+  .directive('partnerScroll', function () {
+    return function (scope, element) {
+      $(element).carouFredSel({
+        items: 4,
+        //width: null,
+        height: 240,
+        direction: "left",
+        transition: true,
+        scroll: {
+          items: 2,
+          easing: "elastic",
+          duration: 500,
+          pauseOnHover: true
+        }
+      });
+    }
+  });
