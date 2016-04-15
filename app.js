3d27440f40e5f238986b3c654fa798f5199d6c6f
ArcheGalla <artempushkar@gmail.com>
2016-04-15 15:44:43 +0300

    add port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = 80;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
