4e908376895f62490c8ec39645dfac58b61163ef
artem <artempushkar@gmail.com>
2017-08-22 09:49:56 +0300

    trying to add webpack support

--- new-version/app.js
+++ new-version/app.js
@@ -11,18 +11,32 @@ const api = require('./api');
 
 const app = express();
 
+const NODE_ENV = process.env.NODE_ENV;
+const port = NODE_ENV === 'development' ? 3000 : 80;
+
+if (NODE_ENV === 'development') {
+    const webpack = require('webpack');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const config = require('./public/webpack.config.js');
+    const compiler = webpack(config);
+
+    app.use(webpackDevMiddleware(compiler, {
+        publicPath: config.output.publicPath
+    }));
+}
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'), dest: path.join(__dirname, 'public'), indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true,
+    src: path.join(__dirname, 'public'), dest: path.join(__dirname, 'public'), indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true,
 }));
 
 app.use(i18n.init);
@@ -31,10 +45,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
 i18n.configure({
-  locales: ['ua', 'den'],
-  directory: __dirname + '/locales',
-  defaultLocale: 'ua',
-  queryParameter: 'lang',  // query parameter to switch locale (ie. /home?lang=ch) - defaults to NULL
+    locales: ['ua', 'den'],
+    directory: __dirname + '/locales',
+    defaultLocale: 'ua',
+    queryParameter: 'lang',  // query parameter to switch locale (ie. /home?lang=ch) - defaults to NULL
 });
 
 app.use('/assets/images', express.static(path.join(__dirname, 'public/images')));
@@ -43,17 +57,17 @@ app.use('stylesheets/assets/images/', express.static(path.join(__dirname, 'publi
 app.use('/', index);
 app.use('/api', api);
 
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.use(function(err, req, res) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (err, req, res) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
