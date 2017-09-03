b5482e82eb4641385b8584415f383c5138e994e6
archegalla <artempushkar@gmail.com>
2017-09-04 02:42:23 +0300

    add latest updates

--- src/app/introduction/introduction.js
+++ src/app/introduction/introduction.js
@@ -1,9 +1,14 @@
-angular.module("app").directive("introduction", [function () {
-  return {
-    restrict: "E",
-    templateUrl: "app/introduction/introduction.html"
-  }
-}]);
+angular
+  .module("app")
+  .directive("introduction", ['$translate', function ($translate) {
+    return {
+      restrict: "E",
+      templateUrl: "app/introduction/introduction.html",
+      link: function (scope) {
 
-
-
+        scope.isLang = function (language) {
+          return $translate.use() === language;
+        }
+      }
+    }
+  }]);
