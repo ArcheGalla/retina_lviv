10b3ef58c523c5a0707510eb58ff955feb40be5f
archegalla <artempushkar@gmail.com>
2017-09-04 02:46:39 +0300

    add latest updates

--- src/app/partners/partners.js
+++ src/app/partners/partners.js
@@ -11,7 +11,7 @@ angular
       $(element).carouFredSel({
         items: 4,
         width: '100%',
-        height: 375,
+        height: 240,
         direction: "left",
         transition: true,
         scroll: {
